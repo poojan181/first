cd4884c52013123f659116e2238123e68605053c
poojan181 <59418259+poojan181@users.noreply.github.com>
2021-01-21 21:32:36 +0530

    Create a.c

---
+++ a.c
@@ -0,0 +1,19 @@
+// This file is a "Hello, world!" in C++ language by GCC for wandbox.
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+    std::cout << "Hello, Wandbox!" << std::endl;
+}
+
+// GCC reference:
+//   https://gcc.gnu.org/
+
+// C++ language references:
+//   https://cppreference.com/
+//   https://isocpp.org/
+//   http://www.open-std.org/jtc1/sc22/wg21/
+
+// Boost libraries references:
+//   https://www.boost.org/doc/
